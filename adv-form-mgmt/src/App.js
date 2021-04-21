1f13af0a945b6244d43dcb6991f7782ce191cb1f
MurrayWarnock <murray.warnock@gmail.com>
2021-04-21 12:44:45 -0600

    Empty React App with h1

---
+++ adv-form-mgmt/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Board User On</h1>
+      </header>
+      <div className="container">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
