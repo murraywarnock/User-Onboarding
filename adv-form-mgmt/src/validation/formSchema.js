4d623ba06222ceb84a4a7680105314eaecebcd32
MurrayWarnock <murray.warnock@gmail.com>
2021-04-21 15:38:43 -0600

    Added better password validation to formSchema.js

--- adv-form-mgmt/src/validation/formSchema.js
+++ adv-form-mgmt/src/validation/formSchema.js
@@ -12,9 +12,11 @@ export default yup.object().shape({
     .required("email is required"),
     password: yup
     .string()
-    // is there yup.password validation??
-    // .password("must be a valid password")
-    .required("password is required"),
+    .required("Please enter your password")
+    .matches(
+      /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+      "Password must contain at least 8 characters, one uppercase, one number and one special case character"
+    ),
 
     // terms checkbox
   terms: yup
